7af3655127c77f719e43862d859d31f0b8fc576c
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-02-19 23:54:43 +0530

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,6 +5,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 
+// Variable that connects us to the Firebase Firestore database
+final _fireStore = FirebaseFirestore.instance;
+
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
   static String id = 'chat_screen';
@@ -14,10 +17,10 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  // Text Controller variable
+  final messageTextController = TextEditingController();
   // Firebase authentication variable
   final _auth = FirebaseAuth.instance;
-  // Variable that connects us to the Firebase Firestore database
-  final _fireStore = FirebaseFirestore.instance;
   // User variable that stores the logged in user.
   late User loggedInUser;
   // String variable that stores the message that the user types
@@ -43,19 +46,20 @@ class _ChatScreenState extends State<ChatScreen> {
         print(loggedInUser.email);
       }
     } catch (e) {
+      // TODO: Implement error handling.
       print(e);
     }
   }
 
   // Returns a stream of all messages (Future objects) in the database
-  void messageStream() async {
-    // Method gets called whenever the database is updated
-    await for (var snapshot in _fireStore.collection('messages').snapshots()) {
-      for (var message in snapshot.docs) {
-        print(message.data());
-      }
-    }
-  }
+  // void messageStream() async {
+  //   // Method gets called whenever the database is updated
+  //   await for (var snapshot in _fireStore.collection('messages').snapshots()) {
+  //     for (var message in snapshot.docs) {
+  //       print(message.data());
+  //     }
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -68,10 +72,8 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: const Icon(Icons.close),
               onPressed: () {
-                messageStream();
-                //Implements logout functionality
-                // _auth.signOut();
-                // exit(0);
+                _auth.signOut();
+                exit(0);
               }),
         ],
 
@@ -86,54 +88,7 @@ class _ChatScreenState extends State<ChatScreen> {
           children: <Widget>[
             // StreamBuilder is a widget which builds itself based on a snapshot from
             // interacting with a Stream.
-            StreamBuilder<QuerySnapshot>(
-              // stream is a QuerySnapshot ->
-              // Contains the results of a query. It can contain zero or more DocumentSnapshot objects.
-                stream: _fireStore.collection('messages').snapshots(),
-                // snapshot is an AsyncSnapshot ->
-                // An immutable representation of the most recent interaction with an
-                // asynchronous computation.
-                builder: (context, snapshot) {
-                  // If no data is recieved
-                  if (!snapshot.hasData) {
-                    return const Center(
-                      child: CircularProgressIndicator(
-                        backgroundColor: Colors.lightBlueAccent,
-                      ),
-                    );
-                  }
-                  // Returns list of DocumentSnapshots, basically all the messages stored in the database
-                  final messages = snapshot.data!.docs;
-                  List<MessageBubble> messageBubbles = [];
-                  // String that stores message text received from the Stream
-                  late String msgText;
-                  // String that stores message sender received from the Stream
-                  late String msgSender;
-                  for (var message in messages) {
-                    // Converts the message.data() object into a Map of strings
-                    final messageMap = message.data() as Map<String, dynamic>;
-                    messageMap.forEach((key, value) {
-                      if (key == 'sender') {
-                        // If sender data is present, put value into msgSender
-                        msgSender = value;
-                      } else if (key == 'text'){
-                        // If text is present, put value into msgText
-                        msgText = value;
-                      }
-                    });
-                    // Creates a Text widget using the text and sender fetched from Firebase
-                    final messageBubble = MessageBubble(sender: msgSender, text: msgText);
-                    messageBubbles.add(messageBubble);
-                  }
-                  // Adds all the Text widgets to the screen in a column.
-                  return Expanded(
-                    child: ListView(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                      children: messageBubbles,
-                    ),
-                  );
-                },
-            ),
+            const MessageBuilder(),
             Container(
               // Custom decoration from constants.dart
               decoration: kMessageContainerDecoration,
@@ -144,6 +99,7 @@ class _ChatScreenState extends State<ChatScreen> {
 
                     // TextField for user entered messages
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
                         // Assign the value of TextField to message variable
                         messageText = value;
@@ -156,6 +112,8 @@ class _ChatScreenState extends State<ChatScreen> {
                   // Send Button
                   TextButton(
                     onPressed: () {
+                      // Clears message TextField when Send button is pressed
+                      messageTextController.clear();
                       // Send the messageText + email to the Firestore database
                       _fireStore.collection('messages').add({
                         'text': messageText,
@@ -177,6 +135,63 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 }
+
+class MessageBuilder extends StatelessWidget {
+  const MessageBuilder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      // stream is a QuerySnapshot ->
+      // Contains the results of a query. It can contain zero or more DocumentSnapshot objects.
+      stream: _fireStore.collection('messages').snapshots(),
+      // snapshot is an AsyncSnapshot ->
+      // An immutable representation of the most recent interaction with an
+      // asynchronous computation.
+      builder: (context, snapshot) {
+        // If no data is received
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+        // Returns list of DocumentSnapshots, basically all the messages stored in the database
+        final messages = snapshot.data!.docs;
+        List<MessageBubble> messageBubbles = [];
+        // String that stores message text received from the Stream
+        late String msgText;
+        // String that stores message sender received from the Stream
+        late String msgSender;
+        for (var message in messages) {
+          // Converts the message.data() object into a Map of strings
+          final messageMap = message.data() as Map<String, dynamic>;
+          messageMap.forEach((key, value) {
+            if (key == 'sender') {
+              // If sender data is present, put value into msgSender
+              msgSender = value;
+            } else if (key == 'text'){
+              // If text is present, put value into msgText
+              msgText = value;
+            }
+          });
+          // Creates a Text widget using the text and sender fetched from Firebase
+          final messageBubble = MessageBubble(sender: msgSender, text: msgText);
+          messageBubbles.add(messageBubble);
+        }
+        // Adds all the Text widgets to the screen in a column.
+        return Expanded(
+          child: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+            children: messageBubbles,
+          ),
+        );
+      },
+    );
+  }
+}
+
 
 class MessageBubble extends StatelessWidget {
   const MessageBubble({
